e4854a37ebcb9ecc6b77be73eb667c832c387188
ChristianK <57948805+ChrisKoll@users.noreply.github.com>
2023-05-19 18:14:26 +0200

    Finished data_handler.R
    Added the possiblity to plot interesting statistics in the cmd and as a PDF file.

--- Project/data_handler.R
+++ Project/data_handler.R
@@ -1,17 +1,3 @@
-# Check for installed packages
-packages <- c("anndata", "edgeR")
-
-all_installed <- TRUE
-for (pkg in packages) {
-    if (!require(pkg, character.only = TRUE, quietly = TRUE)) {
-        
-        all_installed <- FALSE
-        break
-        
-    }
-}
-
-
 # === Functions ================================================================
 
 cpm_normalization <- function(count_data) {
@@ -52,81 +38,134 @@ calc_stats <- function(count_data) {
     max_value <- max(count_data)
     average_value <- mean(count_data)
     
-    frequency <- table(count_data)
-    most_common <- names(frequency)[frequency == max(frequency)]
-    least_common <- names(frequency)[frequency == min(frequency)]
-    
-    summary <- c(min_value, max_value, average_value, most_common, least_common)
+    summary <- c(min_value, max_value, average_value)
     
     return(summary)
 }
 
-plot_stats <- function(counts) {
-    # Gathers various statistical information
+plot_stats <- function(counts, directory) {
+    # Gathers various statistical information and saves them in a pdf file
     
-    summary <- matrix(nrow = 0, ncol = 0)
-    for (matrix in counts) {
-        result <- calc_stats(matrix)
-        summary <- c(summary, result)
+    pdf(file.path(directory, "stats.pdf"), width = 8, height = 11)
+    
+    for (i in 1:length(counts)) {
+        result <- calc_stats(counts[[i]])
+        
+        # Plot table
+        data <- data.frame(
+            Minimum = result[1],
+            Maximum = result[2],
+            Average = result[3]
+        )
+        cat("\n")
+        print(data, row.names = FALSE, quote = FALSE)
+        
+        # Plot bar plot
+        frequency <- as.data.frame(table(counts[i]))
+        barplot(frequency$Freq, 
+                names.arg = frequency$Var1,
+                col = "blue",
+                xlab = "Expression",
+                ylab = "Quantity",
+                main = paste("Counts", as.character(1)))
+        
+        # === Not needed! ===
+        # Plot table
+        # most_common <- names(frequency)[frequency == max(frequency)]
+        # least_common <- names(frequency)[frequency == min(frequency)]
+        
+        # data <- data.frame(
+        #     Most_common = most_common,
+        #     Least_common = least_common
+        # )
+        # print(data)
+        
+        cat("\n")
+        print(paste("Stats computed for", as.character(i), "out of",
+                    as.character(length(counts))),
+              row.names = FALSE, quote = FALSE)
     }
     
-    return(summary)
+    dev.off()
 }
 
 
 # === Script ===================================================================
 
-if (all_installed == TRUE) {
+# Get cmd line arguments
+args <- commandArgs(trailingOnly = TRUE)
     
-    # Load libraries
-    suppressPackageStartupMessages(library(anndata))
-    suppressPackageStartupMessages(library(edgeR))
-    
-    # Get cmd line arguments
-    args <- commandArgs(trailingOnly = TRUE)
-    
-    # Only one arguments can be provided
-    # --> File path
-    if (length(args) == 0) {
+# Check for flags in args
+if ("--path" %in% args) {
         
-        print("Error: No arguments provided...")
-        
-    } else if (length(args) == 1) {
-        
-        file_path <- args
-        # Normalizes the path for every operating system
-        file_path <- normalizePath(path = file_path)
-        
-        # Create anndata object
-        adata <- anndata::read_h5ad(file_path)
-        # Convert anndata to matrix
-        # --> X provides the raw count matrix
-        count_data <- as.matrix(adata$X)
-        
-        cpm_counts <- cpm_normalization(count_data)
-        min0_max1_counts <- min0_max1_nomalization(cpm_counts)
-        # Substitute count matrix with normalized matrix
-        adata$X <- min0_max1_counts
-        
-        counts <- list(count_data, cpm_counts, min0_max1_counts)
-        plot_stats(counts)
-        
-        # Create export file
-        file_name <- paste0("normalized_",
-                            strsplit(basename(args), '\\.')[[1]][1],
-                            ".h5ad")
-        export_path <- file.path(dirname(args), file_name)
-        # Write anndata file
-        write(adata, file = export_path)
+    # Load required packages
+    if (require(anndata)) {
+            
+        if (require(edgeR)) {
+                
+            # Get index of flag in args
+            flag_index <- which(args == "--path")
+                
+            # Check if the flag parameter is provided
+            if (flag_index + 1 <= length(args)) {
+                # Get the value of the flag parameter
+                path_arg <- args[flag_index + 1]
+                    
+            } 
+                
+        } else {
+            
+            print("Error: The package 'edgeR' could not be loaded or attached")
+            
+        }
         
     } else {
         
-        print("Error: Passed too many arguments...")
+        print("Error: The package 'anndata' could not be loaded or attached")
+        
+    }
+        
+}
+        
+if ("--stat" %in% args) {
+    
+    stat_arg <- TRUE
+            
+}
+
+if (file.exists(path_arg)) {
+    
+    # Normalizes the path for every operating system
+    file_path <- normalizePath(path = path_arg)
+    
+    # Create anndata object
+    adata <- anndata::read_h5ad(file_path)
+    # Convert anndata to matrix
+    # --> X provides the raw count matrix
+    count_data <- as.matrix(adata$X)
+    
+    cpm_counts <- cpm_normalization(count_data)
+    min0_max1_counts <- min0_max1_nomalization(cpm_counts)
+    # Substitute count matrix with normalized matrix
+    adata$X <- min0_max1_counts
+    
+    if (stat_arg == TRUE) {
+        
+        counts <- list(count_data, cpm_counts, min0_max1_counts)
+        plot_stats(counts, dirname(file_path))
         
     }
     
+    # Create export file
+    file_name <- paste0("normalized_",
+                        strsplit(basename(file_path), '\\.')[[1]][1],
+                        ".h5ad")
+    export_path <- file.path(dirname(file_path), file_name)
+    # Write anndata file
+    write_h5ad(adata, file = export_path)
+    
 } else {
     
-    print("Error: Not all required packages are installed...")
-    
+    print("Error: The provided path is invalid")
+
 }
