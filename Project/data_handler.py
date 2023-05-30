112ea72e1d8eb78852bf1d5337a37e963e850bd1
Christian <christian.kolland@web.de>
2023-05-30 17:07:13 +0200

    Updated the data_handler class. Added documentation.

--- Project/data_handler.py
+++ Project/data_handler.py
@@ -1,77 +1,46 @@
+# Standard library imports
+from typing import Optional
 from pathlib import Path
+from os import path
 
+# Third-party library imports
 from scanpy import read_h5ad
 from anndata import AnnData
 
 
 class DataHandler:
+    """
+    Class for handling h5ad data.
+    """
 
-    def __init__(self, file_location):
+    def __init__(self, file_location: Optional[str] = None):
+        """Constructor
+
+        :param file_location: Path to h5ad file
+        """
         self.file_location = file_location
         self.adata = self.read_data()
-        self.adata_subset = None
 
-    def read_data(self):
+    def read_data(self) -> AnnData:
+        """
+        Uses the scanpy function read_h5ad to import the anndata object.
+
+        :return: Anndata object
+        """
+        if path.exists(self.file_location) or self.file_location is None:
+            raise ValueError("Invalid or no file path provided.")
+
         annotated_data = read_h5ad(filename=self.file_location)
 
         return annotated_data
 
-    def create_input_vector(self, cell_type: str = None, heart_region: str = None):
-        adata = self.adata
+    def subset_adata(self, export_path: str, number_cols: int = 5000, number_rows: int = 5000):
+        """
+        Subsets a given anndata object to a given size.
 
-        if cell_type is not None:
-            self.adata_subset = adata[adata.obs.cell_type == cell_type]
-
-        elif heart_region is not None:
-            self.adata_subset = adata[adata.obs.region == heart_region]
-
-        else:
-            print('Please provide a method argument...')
-
-    def separate_individual(self):
-        adata = self.adata_subset
-        donors = set(adata.obs.donor)
-
-        print(*donors, sep=', ')
-        chosen_donor = input('Choose a donor: ')
-
-        control_data = adata[adata.obs.donor == chosen_donor]
-        training_data = adata[adata.obs.donor != chosen_donor]
-
-        return control_data, training_data
-
-    def normalization(self):
-        pass
-
-    def subset_adata(self):
-        subset = self.adata[:5000, :5000]
-        path_obj = Path('/media/sf_Share/sample_data.h5ad')
-        subset.write_h5ad(path_obj)
-
-    '''
-    def get_gene_length(self, gene_name: str):
-        Entrez.email = 'christian.kolland@stud.uni-frankfurt.de'
-
-        request = Entrez.epost('gene', id=gene_name)
-        result = Entrez.read(request)
-
-        web_env = result["WebEnv"]
-        query_key = result["QueryKey"]
-        data = Entrez.esummary(db="gene", webenv=web_env, query_key=query_key)
-        print(Entrez.read(data))
-    '''
-
-    '''
-    def get_gene_ids(self):
-        gene_names = self.annotated_data.var_names.tolist()
-
-        mg = mygene.MyGeneInfo()
-        out = mg.querymany(gene_names, scopes='symbol', fields='entrezgene', species='human')
-        print(out)
-    '''
-
-    def show_data(self):
-        print(self.annotated_data.obs_names[:10].tolist())
-        # print(self.annotated_data.obs_names[-10:].tolist())
-        print(self.annotated_data.var_names[:10].tolist())
-        # print(self.annotated_data.var_names[-10:].tolist())
+        :param export_path: Path for the file to be exported
+        :param number_cols: Number of columns kept in the subset
+        :param number_rows: Number of rows kept in the subset
+        """
+        subset = self.adata[:number_cols, :number_rows]
+        subset.write_h5ad(Path(export_path))
